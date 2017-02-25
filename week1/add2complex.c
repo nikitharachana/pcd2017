c769c0f12a2b7f3f2de7bb1a2bc75ee31326d7d5
Your Name <nira16is@cmrit.ac.in>
2017-02-25 12:51:58 +0530

    hello5

---
+++ week1/add2complex.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+struct complex
+{
+float real;
+float img;
+}
+struct complex add(struct complex 
+a,struct complex b)
+{
+struct complex c;
+c.real=a.real+b.real;
+c.img=a.img+b.img;
+return c;
+}
+int main()
+{
+struct complex a,b,c;
+printf("Enter the real value of 
+a=");
+scanf("%f",&a.real);
+printf(Enter the img value of a=");
+scanf("%f",&a.img);
+printf(Enter the real value of 
+b=");
+scanf("%f",&b.real);
+printf(Enter the img value of b=");
+scanf("%f",&b.img);
+c=add(a,b);
+printf("Sum of the two complex 
+numbers=%f+i%f\n",c.real,c.img);
+return 0;
+}
+
